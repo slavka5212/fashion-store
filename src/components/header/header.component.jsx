3bb8f2894d4b29fa912dc3b388c224b2fab8cd57
slavka5212 <slavka5212@gmail.com>
2020-04-11 13:59:22 +0200

    change style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/bloom.svg';
 
 import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      <Logo className='logo' /> <div className='logo-title'>Fashion Store</div>
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
